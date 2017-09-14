d96e03400e3c78aca5e317e64c9d28962c318727
Jacob Herper <j.herper@creativ.agency>
2017-09-14 12:02:07 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import GuestList from './GuestList';
+
+class App extends Component {
+  state = {
+    guests: [
+      {
+        name: "Jacob",
+        isConfirmed: false
+      },
+      {
+        name: "John",
+        isConfirmed: true
+      }
+    ]
+  }
+  getTotalInvited = () => this.state.guests.length;
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>Event Management App</p>
+          <form>
+              <input type="text" value="Safia" placeholder="Invite Someone" />
+              <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList guests={this.state.guests} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
