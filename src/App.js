d1a4e99efd4f621ec943ff7e5b2abf6e62984693
Jacob Herper <j.herper@creativ.agency>
2017-09-14 13:32:09 +0200

    Adding and removing guests added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GuestList from './GuestList';
 class App extends Component {
   state = {
     isFiltered: false,
+    pendingGuest: "",
     guests: [
       {
         name: "Jacob",
@@ -39,6 +40,14 @@ class App extends Component {
   toggleConfirmationAt = index =>
       this.toggleGuestPropertyAt("isConfirmed", index);
 
+  removeGuestAt = index =>
+    this.setState({
+      guests: [
+        ...this.state.guests.slice(0, index),
+        ...this.state.guests.slice(index + 1)
+      ]
+    });
+
   toggleEditingAt = index =>
       this.toggleGuestPropertyAt("isEditing", index);
 
@@ -58,6 +67,24 @@ class App extends Component {
   toggleFilter = () =>
     this.setState({ isFiltered: !this.state.isFiltered });
 
+  handleNameInput = e =>
+    this.setState({ pendingGuest: e.target.value });
+
+  newGuestSubmitHandler =e => {
+    e.preventDefault();
+    this.setState({
+      guests: [
+        {
+          name: this.state.pendingGuest,
+          isConfirmed: false,
+          isEditing: false
+        },
+        ...this.state.guests
+      ],
+      pendingGuest: ''
+    })
+  }
+
   getTotalInvited = () => this.state.guests.length;
 
   render() {
@@ -66,9 +93,19 @@ class App extends Component {
         <header>
           <h1>RSVP</h1>
           <p>Event Management App</p>
-          <form>
-              <input type="text" value="Safia" placeholder="Invite Someone" />
-              <button type="submit" name="submit" value="submit">Submit</button>
+          <form
+            onSubmit={this.newGuestSubmitHandler}>
+              <input
+                type="text"
+                onChange={this.handleNameInput}
+                value={this.state.pendingGuest}
+                placeholder="Invite Someone"
+              />
+              <button
+                type="submit"
+                name="submit"
+                value="submit"
+              >Submit</button>
           </form>
         </header>
         <div className="main">
@@ -104,6 +141,7 @@ class App extends Component {
             toggleEditingAt={this.toggleEditingAt}
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
+            removeGuestAt={this.removeGuestAt}
           />
         </div>
       </div>
--- src/Guest.js
+++ src/Guest.js
@@ -19,7 +19,7 @@ const Guest = props =>
     <button onClick={props.handleToggleEditing}>
       {props.isEditing ? "save" : "edit"}
     </button>
-    <button>remove</button>
+    <button onClick={props.handleRemove}>remove</button>
   </li>
 
 Guest.PropTypes = {
@@ -28,7 +28,8 @@ Guest.PropTypes = {
   isEditing: PropTypes.bool.isRequired,
   handleConfirmation: PropTypes.func.isRequired,
   handleToggleEditing: PropTypes.func.isRequired,
-  setName: PropTypes.func.isRequired
+  setName: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired
 }
 
 export default Guest;
